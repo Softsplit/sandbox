c4af8cdf148dca73e6295ed70bce44558e57f04f
Asphaltian <86578963+Asphaltian@users.noreply.github.com>
2024-07-13 19:36:10 +0800

    Mount packages automatically when someone joins

--- Code/GameManager.cs
+++ Code/GameManager.cs
@@ -1,3 +1,4 @@
+using Sandbox.Services;
 using System.Threading.Tasks;
 
 public partial class GameManager : Component, Component.INetworkListener
@@ -71,8 +72,18 @@ public partial class GameManager : Component, Component.INetworkListener
 		*/
 	}
 
+	[HostSync] private NetList<string> cachedPackageList { get; set; } = new();
+
+	void INetworkListener.OnActive( Connection conn )
+	{
+		foreach ( var packageName in cachedPackageList )
+		{
+			MountPackage( packageName );
+		}
+	}
+
 	[Broadcast]
-	public async void MountPackage( string packageName )
+	public static async void MountPackage( string packageName )
 	{
 		await MountPackageAsync( packageName );
 	}
@@ -111,7 +122,11 @@ public partial class GameManager : Component, Component.INetworkListener
 
 		if ( modelname.Contains( "." ) && !modelname.EndsWith( ".vmdl", System.StringComparison.OrdinalIgnoreCase ) && !modelname.EndsWith( ".vmdl_c", System.StringComparison.OrdinalIgnoreCase ) )
 		{
-			Current.MountPackage( modelname );
+			MountPackage( modelname );
+
+			if ( !Current.cachedPackageList.Contains( modelname ) )
+				Current.cachedPackageList.Add( modelname );
+
 			model = Model.Load( await MountPackageAsync( modelname ) );
 		}
 		else
@@ -148,7 +163,7 @@ public partial class GameManager : Component, Component.INetworkListener
 		ent.NetworkSpawn();
 		ent.Network.DropOwnership();
 
-		Sandbox.Services.Stats.Increment( "spawn.model", 1, modelname );
+		Stats.Increment( "spawn.model", 1, modelname );
 	}
 
 	[ConCmd( "spawn_entity" )]
