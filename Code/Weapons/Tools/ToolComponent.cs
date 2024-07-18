ccfa65fa5f0b61f48290a52ea5605769d279d54f
SwagAccount <160753124+SwagAccount@users.noreply.github.com>
2024-07-18 22:37:11 +0700

    Add weld tool (#22)
    * Added A Basic Tool System
    
    * Changed OnFixedUpdate to OnInputUpdate
    
    * Reverted back to onfixed update, as for some reason oninputupdate isn't being called
    
    * Fixed some stuff
    
    * work please?
    
    * please <3 sbox and gitgub
    
    * Cleaning up
    
    * Added weld
    
    * Added weld
    
    ---------
    
    Co-authored-by: Asphaltian <86578963+Asphaltian@users.noreply.github.com>

--- Code/Weapons/Tools/ToolComponent.cs
+++ Code/Weapons/Tools/ToolComponent.cs
@@ -7,6 +7,8 @@ public abstract class ToolComponent : InputWeaponComponent
 {
 	ToolGunHandler toolGunHandler;
 
+	public Ray WeaponRay => Equipment.Owner.AimRay;
+  
 	protected override void OnStart()
 	{
 		InputActions.Add( "Attack2" );
@@ -24,16 +26,28 @@ public abstract class ToolComponent : InputWeaponComponent
 			SecondaryAction();
 
 		if ( Input.Pressed( "ToolGunMenu" ) )
+
 			toolGunHandler.ActiveToolMenu.Enabled = !toolGunHandler.ActiveToolMenu.Enabled;
 	}
 
 	protected virtual void PrimaryAction()
 	{
-
+		
 	}
 
 	protected virtual void SecondaryAction()
 	{
+		
+	}
 
+	[Broadcast]
+	public void Recoil()
+	{
+		Sound.Play( "sounds/guns/gun_dryfire.sound", Transform.Position );
+		if ( Equipment.Owner.IsValid() && Equipment.Owner.BodyRenderer.IsValid() )
+			Equipment.Owner.BodyRenderer.Set( "b_attack", true );
+
+		if ( Equipment.ViewModel.IsValid() )
+			Equipment.ViewModel.ModelRenderer.Set( "b_attack", true );
 	}
 }
