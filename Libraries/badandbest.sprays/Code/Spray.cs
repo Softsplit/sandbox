9ad03e1b516f0ea119affee6c28e502f372d29dd
Asphaltian <86578963+Asphaltian@users.noreply.github.com>
2024-07-26 16:12:51 +0800

    Update sprays library

--- Libraries/badandbest.sprays/Code/Spray.cs
+++ Libraries/badandbest.sprays/Code/Spray.cs
@@ -1,5 +1,6 @@
 using Sandbox;
 using Sandbox.Utility;
+using System;
 
 namespace badandbest.Sprays;
 
@@ -8,10 +9,24 @@ namespace badandbest.Sprays;
 /// </summary>
 public static class Spray
 {
-	[ConVar( "spray", Help = "URL of image. Must be in quotes.", Saved = true )]
-	private static string ImageUrl { get; set; }
-
 	private static GameObject LocalSpray;
+
+	[ConVar( "spray", Help = "URL of image. Must be in quotes.", Saved = true )]
+	internal static string Image { get; set; }
+
+	[ConVar( "spraydisable", Help = "Disables player sprays. Good for streamers.", Saved = true ), Change]
+	internal static bool Disabled { get; set; }
+
+	/// <summary>
+	/// Toggles all existing sprays when <see cref="Disabled"/> changes.
+	/// </summary>
+	private static void OnDisabledChanged( bool _, bool disabled )
+	{
+		foreach ( var spray in Game.ActiveScene.Components.GetAll<SprayRenderer>( FindMode.InChildren ) )
+		{
+			spray.Enabled = !disabled;
+		}
+	}
 
 	/// <summary>
 	/// Places an image on a surface.
@@ -36,14 +51,14 @@ public static class Spray
 		if ( trace.Run() is not { Body.BodyType: PhysicsBodyType.Static } tr )
 			return;
 
-		if ( string.IsNullOrEmpty( ImageUrl ) )
-			ImageUrl = "materials/decals/default.png";
+		if ( string.IsNullOrEmpty( Image ) )
+			Image = "materials/decals/default.png";
 
 		var config = new CloneConfig
 		{
 			Name = $"Spray - {Steam.PersonaName}",
 			Transform = new( tr.HitPosition, Rotation.LookAt( tr.Normal ) ),
-			PrefabVariables = new() { { "Image", ImageUrl } }
+			PrefabVariables = new() { { "Image", Image } }
 		};
 
 		LocalSpray?.Destroy();
@@ -57,23 +72,42 @@ public static class Spray
 [Title( "Spray Renderer" ), Icon( "imagesearch_roller" )]
 internal class SprayRenderer : Renderer
 {
+	private DecalRenderer decal;
+
 	[Property, ImageAssetPath]
 	public string Image { get; set; }
 
-	protected override async void OnEnabled()
+	protected override async void OnAwake()
 	{
-		var texture = await Texture.LoadAsync( FileSystem.Mounted, Image, false );
+		decal = Components.Get<DecalRenderer>( FindMode.InChildren );
+		decal.Enabled = !Spray.Disabled;
 
-		if ( texture is null or { Width: <= 32, Height: <= 32 } )
+		Texture texture = null;
+
+		try
 		{
-			// Probably an error texture. Replace with a fallback image.
-			Components.Get<DecalRenderer>( FindMode.InChildren ).Material = Material.Load( "materials/fallback.vmat" );
-			return;
+			texture = await Texture.LoadAsync( FileSystem.Mounted, Image, false );
+			if ( texture is null or { Width: <= 32, Height: <= 32 } )
+			{
+				// Probably an error texture. Replace with a fallback image.
+				texture = await Texture.LoadAsync( FileSystem.Mounted, "materials/fallback.vtex" );
+			}
 		}
+		catch ( Exception e )
+		{
+			Log.Error( $"Couldn't Load Avatar {Image} {e}" );
+			throw;
+		}
+		finally
+		{
+			var material = Material.Load( "materials/spray.vmat" ).CreateCopy();
+			material.Set( "g_tColor", texture );
 
-		var material = Material.Load( "materials/spray.vmat" ).CreateCopy();
-		material.Set( "g_tColor", texture );
-
-		Components.Get<DecalRenderer>( FindMode.InChildren ).Material = material;
+			decal.Material = material;
+		}
 	}
+
+	protected override void OnEnabled() => decal.Enabled = true;
+
+	protected override void OnDisabled() => decal.Enabled = false;
 }
