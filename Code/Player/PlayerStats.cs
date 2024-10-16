72f1eb5affbd4ad7a52c45267cb547ee862bf2b6
Asphaltian <86578963+Asphaltian@users.noreply.github.com>
2024-10-16 23:34:46 +0800

    Marathons

--- Code/Player/PlayerStats.cs
+++ Code/Player/PlayerStats.cs
@@ -6,6 +6,21 @@ public sealed class PlayerStats : Component, Component.INetworkListener, IPlayer
 	[RequireComponent] public PlayerController PlayerController { get; set; }
 	[RequireComponent] public Player Player { get; set; }
 
+	RealTimeUntil timeUntilHalfMarathon = 14400;
+	RealTimeUntil timeUntilMarathon = 28800;
+
+	protected override void OnUpdate()
+	{
+		if ( IsProxy )
+			return;
+
+		if ( timeUntilHalfMarathon )
+			Sandbox.Services.Achievements.Unlock( "half_marathon" );
+
+		if ( timeUntilMarathon )
+			Sandbox.Services.Achievements.Unlock( "marathon" );
+	}
+
 	void IPlayerEvent.OnSpawned( Player player )
 	{
 		if ( player != Player )
