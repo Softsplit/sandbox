f81edbb9b68a15c3a433f6f9226bdfdebf8be808
thehumbleonion <160753124+SwagAccount@users.noreply.github.com>
2024-11-02 17:14:28 +0000

    Fix bytepack error

--- Code/Components/Base/BaseWeapon.cs
+++ Code/Components/Base/BaseWeapon.cs
@@ -158,15 +158,27 @@ public partial class BaseWeapon : Component
 
 	public static LegacyParticleSystem CreateParticleSystem( string path, Transform transform, float time = 1, GameObject parent = null )
 	{
-		SpawnParticleSystem( path, transform, time, parent );
+		SpawnParticleSystem( Connection.Local.Id, path, transform.Position, transform.Rotation, time, parent );
+		return MakeParticleSystem( path, transform.Position, transform.Rotation, time, parent );
+	}
 
+	[Broadcast]
+	public static void SpawnParticleSystem( Guid connection, string path, Vector3 position, Rotation rotation, float time = 1, GameObject parent = null )
+	{
+		if ( Connection.Local.Id == connection )
+			return;
+		MakeParticleSystem( path, position, rotation, time, parent );
+	}
+
+	public static LegacyParticleSystem MakeParticleSystem( string path, Vector3 position, Rotation rotation, float time = 1, GameObject parent = null )
+	{
 		var particleSystem = ParticleSystem.Load( path );
 
 		var go = new GameObject
 		{
 			Name = particleSystem.Name,
 			Parent = parent,
-			WorldTransform = transform
+			WorldTransform = new Transform( position, rotation )
 		};
 
 		var legacyParticleSystem = go.AddComponent<LegacyParticleSystem>();
@@ -180,32 +192,6 @@ public partial class BaseWeapon : Component
 			go.DestroyAsync( time );
 
 		return legacyParticleSystem;
-	}
-
-	[Broadcast]
-	public static void SpawnParticleSystem( string path, Transform transform, float time = 1, GameObject parent = null )
-	{
-		if ( !IsProxy )
-			return;
-
-		var particleSystem = ParticleSystem.Load( path );
-
-		var go = new GameObject
-		{
-			Name = particleSystem.Name,
-			Parent = parent,
-			WorldTransform = transform
-		};
-
-		var legacyParticleSystem = go.AddComponent<LegacyParticleSystem>();
-		legacyParticleSystem.Particles = particleSystem;
-		legacyParticleSystem.ControlPoints = new()
-		{
-			new ParticleControlPoint { GameObjectValue = go, Value = ParticleControlPoint.ControlPointValueInput.GameObject }
-		};
-
-		if ( time > 0 )
-			go.DestroyAsync( time );
 	}
 
 	public virtual bool CanReload()
